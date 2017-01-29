ccfdb171b7d1679dd5088caf53792538d85f80c6
Lo <hundsimma@gmail.com>
2017-01-29 02:09:55 +0100

    Add skeleton for filtering by transportation means.

--- app/controllers/fetch_trips_controller.rb
+++ app/controllers/fetch_trips_controller.rb
@@ -2,8 +2,10 @@ class FetchTripsController < ApplicationController
   def index
   	origin 					= params['originId']
   	destination 		= params['destinationId']
+  	transportation_means = params['transportation_means']
   	fetched_data 		= FetchExternalData.call(origin: origin, destination: destination)
   	@filtered_data 	= FilterExternalData.call(fetched_data)
+  	@filtered_data 	= IncludeOnlyTransportation.call(@filtered_data, transportation_means) if transportation_means.present?
   	render status: :success
   end
 end
---
+++ app/services/include_only_transportation.rb
@@ -0,0 +1,8 @@
+class FilterExternalData
+	include Service
+
+	def call(filtered_data, transportation_means)
+		# filter data by transportation means
+		filtered_data
+	end
+end
